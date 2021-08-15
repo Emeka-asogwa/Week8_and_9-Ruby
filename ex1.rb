124c7ffcb282d0f52909a06b90bb2e55d05d276d
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-08-15 16:21:46 -0400

    ✍🏽 Implement exercises 1,2,3 and 4

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+# This is to output (puts) a string in Ruby. 
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
